7c6bc7d34d27c2299747bfeeabf60dc4e0663ef6
JEALG <jalain.gosse@icloud.com>
2024-01-13 10:13:40 +0100

    amélioration affichage + lien community

--- desktop/php/songs.php
+++ desktop/php/songs.php
@@ -8,7 +8,7 @@ sendVarToJS('eqType', $plugin->getId());
 
 <div class="row row-overflow">
 	<div class="col-xs-12 songThumbnailDisplay">
-		<legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
+		<legend><i class="fas fa-cog"></i> {{Gestion}}</legend>
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor songAction logoPrimary" data-action="add">
 				<i class="fas fa-plus-circle"></i>
@@ -20,12 +20,23 @@ sendVarToJS('eqType', $plugin->getId());
 				<br>
 				<span>{{Configuration}}</span>
 			</div>
+			<?php
+			$jeedomVersion  = jeedom::version() ?? '0';
+			$displayInfo = version_compare($jeedomVersion, '4.4.0', '>=');
+			if ($displayInfo) {
+				echo '<div class="cursor eqLogicAction info" data-action="createCommunityPost">';
+				echo '<i class="fas fa-ambulance"></i><br>';
+				echo '<span>{{Community}}</span>';
+				echo '</div>';
+			}
+			?>
 		</div>
 		<legend><i class='fas fa-music'></i> {{Mes sons}}</legend>
-		<div class="input-group" style="margin:5px;">
-			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchSong"/>
+		<div class="input-group" style="margin-bottom:5px;">
+			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
 			<div class="input-group-btn">
-				<a id="bt_resetSearch" class="btn roundedRight" style="width:30px"><i class="fas fa-times"></i></a>
+				<a id="bt_resetObjectSearch" class="btn" style="width:30px"><i class="fas fa-times"></i>
+				</a><a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
 			</div>
 		</div>
 		<div class="eqLogicThumbnailContainer">
@@ -44,7 +55,8 @@ sendVarToJS('eqType', $plugin->getId());
 	<div class="col-xs-12 song" style="display: none;">
 		<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
-				<a class="btn btn-sm btn-success songAction roundedLeft" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm songAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+				<a class="btn btn-sm btn-success songAction roundedLeft" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}
+				</a><a class="btn btn-danger btn-sm songAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
 			</span>
 		</div>
 		<ul class="nav nav-tabs" role="tablist">
@@ -53,42 +65,43 @@ sendVarToJS('eqType', $plugin->getId());
 		</ul>
 		<div class="tab-content">
 			<div role="tabpanel" class="tab-pane active" id="songtab">
-				<br/>
-				<div class="row">
-					<div class="col-lg-7">
-						<form class="form-horizontal">
-							<fieldset>
-								<legend><i class="fas fa-wrench"></i> {{Général}}</legend>
-								<div class="form-group">
-									<label class="col-sm-3 control-label">{{Nom du son}}</label>
-									<div class="col-xs-11 col-sm-7">
-										<input type="text" class="songAttr form-control" data-l1key="id" style="display : none;" />
-										<input type="text" class="songAttr form-control" data-l1key="name" placeholder="{{Nom du son}}"/>
-									</div>
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="col-lg-6">
+							<legend><i class="fas fa-wrench"></i> {{Général}}</legend>
+							<div class="form-group">
+								<label class="col-sm-4 control-label">{{Nom du son}}</label>
+								<div class="col-xs-11 col-sm-7">
+									<input type="text" class="songAttr form-control" data-l1key="id" style="display : none;" />
+									<input type="text" class="songAttr form-control" data-l1key="name" placeholder="{{Nom du son}}" />
 								</div>
-								<div class="form-group">
-									<label class="col-sm-3 control-label">{{ID}}</label>
-									<div class="col-xs-11 col-sm-7">
-										<input type="text" class="songAttr form-control" data-l1key="logicalId" />
-									</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-4 control-label">{{ID}}</label>
+								<div class="col-xs-11 col-sm-7">
+									<input type="text" class="songAttr form-control" data-l1key="logicalId" />
 								</div>
-								<div class="form-group">
-									<label class="col-sm-3 control-label">{{Sons}}</label>
-									<div class="col-sm-7 col-xs-11">
-										<span class="btn btn-default btn-file">
-											<i class="fas fa-cloud-upload-alt"></i> {{Envoyer}}<input  id="bt_uploadSong" type="file" name="file" style="display: inline-block;">
-										</span>
-									</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-4 control-label">{{Sons}}</label>
+								<div class="col-sm-7 col-xs-11">
+									<span class="btn btn-default btn-file">
+										<i class="fas fa-cloud-upload-alt"></i> {{Envoyer}}<input id="bt_uploadSong" type="file" name="file" style="display: inline-block;">
+									</span>
 								</div>
-							</fieldset>
-						</form>
+							</div>
 
-					</div>
-				</div>
+						</div>
+
+					</fieldset>
+
+				</form>
 			</div>
 		</div>
 	</div>
 </div>
 
-<?php include_file('core', 'songs', 'class.js', 'songs');?>
-<?php include_file('desktop', 'songs', 'js', 'songs');?>
+<?php include_file('core', 'songs', 'js', 'songs'); ?>
+<?php include_file('core', 'plugin.template', 'js'); ?>
+<?php include_file('core', 'songs', 'class.js', 'songs'); ?>
+<?php include_file('desktop', 'songs', 'js', 'songs'); ?>
